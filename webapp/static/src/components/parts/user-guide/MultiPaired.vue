19e5bf6af2885cd4cf2ea7c66d4406ee790ea9ec
Joses W. Ho <joseshowh@gmail.com>
2017-11-29 13:17:25 +0800

    add user guide (WIP)

--- webapp/static/src/components/parts/user-guide/MultiPaired.vue
+++ webapp/static/src/components/parts/user-guide/MultiPaired.vue
@@ -1,6 +1,12 @@
 <template>
 	<div>
-		Put user guide for Multi-paired plot here.
+		To compare two or more similar experiments, it is often convenient to place a number of two-groups results into a single arrangement. In this plot, the mean difference can no longer be placed to the right. Instead, the two or more mean differences are plotted below the observed values axes in new delta axes. This allows the direct visual comparison of multiple mean differences (‘deltas’).
+
+		<br><br>
+		Since there are two measurements for each individual, there needs to be two columns of numbers <i>of the same length</i>.
+
+		<p><i>Example figure coming soon!</i></p>
+
 	</div>
 </template>
 
--- webapp/static/src/components/parts/user-guide/SharedControl.vue
+++ webapp/static/src/components/parts/user-guide/SharedControl.vue
@@ -1,6 +1,8 @@
 <template>
 	<div>
-		Put user guide for Shared control plot here.
+		Many biological experiments are designed around a single control group and its comparison with several intervention groups. This plot calculates and plots the mean differences between a single control (the first column of your spreadsheet) and each of the intervention groups.
+
+		<p><i>Example figure coming soon!</i></p>
 	</div>
 </template>
 

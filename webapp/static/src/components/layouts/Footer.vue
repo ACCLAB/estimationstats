ab3cc1f680ff89a53d2630edcfb17c4e45ff226d
Joses W. Ho <joseshowh@gmail.com>
2018-02-06 14:16:36 +0800

    layout modifications

--- webapp/static/src/components/layouts/Footer.vue
+++ webapp/static/src/components/layouts/Footer.vue
@@ -1,9 +1,9 @@
 <template>
 	<footer>
 		<div class="row">
-			<div class="col s12">
+			<!-- <div class="col s12">
 				<div class="divider heavy"></div>
-			</div>
+			</div> -->
 		</div>
 		<div class="row valign-wrapper">
 			<div class="col s2 center-align">
@@ -11,15 +11,21 @@
 					<img class="logo" src="./../../../static/img/Logo-resize.svg">
 				</router-link>
 			</div>
-			<div class="col s10">
+			<div class="col s12">
 				<div class="menu center-align">
+
 					<router-link exact :to="{ name: 'analyze', params: { plotType: plotTypes.UNPAIRED.type }}" class="grey-text text-darken-1">{{ plotTypes.UNPAIRED.name }}</router-link>
+ 
 					<span class="inline-divider heavy"></span>
 					<router-link exact :to="{ name: 'analyze', params: { plotType: plotTypes.PAIRED.type }}" class="grey-text text-darken-1">{{ plotTypes.PAIRED.name }}</router-link>
+
 					<span class="inline-divider heavy"></span>
 					<router-link exact :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI.type }}" class="grey-text text-darken-1">{{ plotTypes.MULTI.name }}</router-link>
+
 					<span class="inline-divider heavy"></span>
 					<router-link exact :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI_PAIRED.type }}" class="grey-text text-darken-1">{{ plotTypes.MULTI_PAIRED.name }}</router-link>
+
+
 					<span class="inline-divider heavy hide-on-med-and-down"></span>
 					<router-link exact :to="{ name: 'analyze', params: { plotType: plotTypes.SHARED_CONTROL.type }}" class="grey-text text-darken-1">{{ plotTypes.SHARED_CONTROL.name }}</router-link>
 				</div>

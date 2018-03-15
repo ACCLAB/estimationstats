74be09a28aa2369d8fcc7ab7a52c1ddd6f4293a1
Joses W. Ho <joseshowh@gmail.com>
2018-03-15 14:10:53 +0800

    landing page re-org

--- webapp/static/src/components/pages/Home.vue
+++ webapp/static/src/components/pages/Home.vue
@@ -1,11 +1,11 @@
 <template>
 	<div id="home">
 		<div class="row"></div>
-		<!-- <div class="row">
+		<div class="row">
 			<div class="col s6 offset-s6">
 				<img width="100%" src="./../../../static/img/Curve.svg">
 			</div>
-		</div> -->
+		</div>
 		<div class="row">
 			<div class="col s12">
 				<span class="title main-title grey-text text-darken-3">
@@ -31,11 +31,11 @@
 					What is Estimation Statistics?
 				</router-link>
 			</div>
-			<div class="col s6">
+			<!-- <div class="col s6">
 				<router-link :to="{ name: 'user-guide', params: { plotType: plotTypes.UNPAIRED.type }}">
 					The Five Plot Types
 				</router-link>
-			</div>
+			</div> -->
 		</div>
 
 
@@ -44,61 +44,44 @@
 		<br>
 		<div class="row">
 			<div class="col s12">
-				<span id="plot" class="title">Gardner-Altman Plots</span>
+				<span id="plot" class="title">Plot</span>
 				<div class="divider heavy"></div>
 			</div>
 			<div class="col s6">
 				<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.UNPAIRED.type }}">
-					<!-- <img src="./../../../static/img/two-group-unpaired-display.svg" height="50%" width="50%">
-					<br> -->
 					{{ plotTypes.UNPAIRED.name }}
 				</router-link>
 			</div>
 
 			<div class="col s6">
 				<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.PAIRED.type }}">
-					<!-- <img src="./../../../static/img/two-group-paired-display.svg" height="50%" width="50%">
-					<br> -->
 					{{ plotTypes.PAIRED.name }}
 				</router-link>
 			</div>
 		</div>
 
-			<div class="row"></div>
-			<br>
+		<div class="row">
+			<div class="col s6">
+				<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI.type }}">
+					{{ plotTypes.MULTI.name }}
+				</router-link>
+			</div>
 
-			<div class="row">
-				<div class="col s12">
-					<span id="plot" class="title">Cumming Plots</span>
-					<div class="divider heavy"></div>
-				</div>
-				<!-- <div class="row"></div> Not sure why a spacer row is needed here..-->
-				<div class="col s6">
-					<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI.type }}">
-						<!-- <img src="./../../../static/img/multi-two-group-unpaired-display.svg" height="75%">
-						<br> -->
-						{{ plotTypes.MULTI.name }}
-					</router-link>
-				</div>
+			<div class="col s6">
+				<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI_PAIRED.type }}">
+					{{ plotTypes.MULTI_PAIRED.name }}
+				</router-link>
+			</div>
+		</div>
 
-				<div class="col s6">
-					<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.MULTI_PAIRED.type }}">
-						<!-- <img src="./../../../static/img/multi-two-group-paired-display.svg" height="75%">
-						<br> -->
-						{{ plotTypes.MULTI_PAIRED.name }}
-					</router-link>
-				</div>
-
-			<!-- <div class="row"></div> -->
-			<!-- <div class="row"></div> -->
+		<div class="row">
 			<div class="col s6">
 				<router-link :to="{ name: 'analyze', params: { plotType: plotTypes.SHARED_CONTROL.type }}">
-					<!-- <img src="./../../../static/img/shared-control-display.svg" height="75%">
-					<br> -->
 					{{ plotTypes.SHARED_CONTROL.name }}
 				</router-link>
 			</div>
 		</div>
+
 
 		<div class="row"></div>
 		<br>

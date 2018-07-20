5e5bf1343020de4bccabf6b06662839f606b3369
Joses W. Ho <joseshowh@gmail.com>
2018-07-20 18:06:21 +0800

    add reference to reticulate tutorial

--- webapp/static/src/components/pages/GetCode.vue
+++ webapp/static/src/components/pages/GetCode.vue
@@ -3,7 +3,11 @@
 		<h5>The DABEST package</h5>
 		<p>
 		If you are a power user, you can install the DABEST package for further customization of your esimation plots.</p>
-		<p>The package is available on Github for <a href='https://github.com/ACCLAB/DABEST-python' target='_blank'>Python</a> and <a href='https://github.com/ACCLAB/DABEST-Matlab' target='_blank'>Matlab</a>; an R version is under development.
+		<p>The package is available on Github for <a href='https://github.com/ACCLAB/DABEST-python' target='_blank'>Python</a> and <a href='https://github.com/ACCLAB/DABEST-Matlab' target='_blank'>Matlab</a>.
+
+		<br><br>
+		 There are no plans to port the package to R. However, you can use the `reticulate` package to run Python code inside an R console, or in R Markdown.
+		 <br>A tutorial can be found <a href='https://acclab.github.io/DABEST-python-docs/dabest-r.html' target='blank'>here</a>.
 	</p>
 	</div>
 </template>

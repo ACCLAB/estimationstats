ee7e9ec42c1626dc065aabdc14f5fe972ad200a4
Joses W. Ho <joseshowh@gmail.com>
2018-11-27 15:49:34 +0800

    edit About layout

--- webapp/static/src/components/pages/About.vue
+++ webapp/static/src/components/pages/About.vue
@@ -2,18 +2,18 @@
 	<div>
 		<h5>The authors</h5>
 		<p>
-			<img src="../../../static/img/people/adam.png" align="left" height="200px" style="margin: 10px 10px 0px"/>
-			As a behavioral scientist, <a href ='https://twitter.com/adamcchang'>Adam Claridge-Chang</a> became frustrated with significance testing; he retrained in estimation statistics by reading textbooks and articles by <a href='http://as.wiley.com/WileyCDA/WileyTitle/productCd-0727913751.html' target='_blank'>Altman</a>, <a href='https://effectsizefaq.com/about/' target='_blank'>Ellis</a>, <a href='https://www.routledge.com/Introduction-to-the-New-Statistics-Estimation-Open-Science-and-Beyond/Cumming-Calin-Jageman/p/book/9781138825529' target='_blank'>Cumming</a>, <a href='https://www.ics.uci.edu/~sternh/courses/210/cohen94_pval.pdf' target='_blank'>Cohen</a>, and <a href='http://as.wiley.com/WileyCDA/WileyTitle/productCd-EHEP002313.html'>Borenstein</a>. Noting that the suite of methods went by several names, Adam wrote a <a href='https://en.wikipedia.org/wiki/Estimation_statistics'>Wikipedia</a> page and published an <a href='https://doi.org/10.5281/zenodo.60156'>editorial</a> branding the framework as <em>estimation statistics</em>.
+			<img src="../../../static/img/people/adam.png" align="left" height="200px" style="margin: 10px 30px 0px 0px"/>
+			As a behavioral scientist, <a href ='https://twitter.com/adamcchang'>Adam Claridge-Chang</a> became frustrated with significance testing; he retrained in estimation statistics by reading textbooks and articles by <a href='http://as.wiley.com/WileyCDA/WileyTitle/productCd-0727913751.html' target='_blank'>Altman</a>, <a href='https://effectsizefaq.com/about/' target='_blank'>Ellis</a>, <a href='https://www.routledge.com/Introduction-to-the-New-Statistics-Estimation-Open-Science-and-Beyond/Cumming-Calin-Jageman/p/book/9781138825529' target='_blank'>Cumming</a>, <a href='https://www.ics.uci.edu/~sternh/courses/210/cohen94_pval.pdf' target='_blank'>Cohen</a>, and <a href='http://as.wiley.com/WileyCDA/WileyTitle/productCd-EHEP002313.html'>Borenstein</a>. Noting that the suite of methods went by several names, Adam wrote a <a href='https://en.wikipedia.org/wiki/Estimation_statistics'>Wikipedia entry</a> and published an <a href='https://doi.org/10.5281/zenodo.60156'>editorial</a> branding the framework as <em>estimation statistics</em>.
 		</p>
-
+		<br>
 		<p>
-			<img src="../../../static/img/people/joses.jpg" align="left" height="170px" style="margin: 10px 10px 0px"/>
+			<img src="../../../static/img/people/joses.jpg" align="left" height="170px" style="margin: 10px 30px 0px 0px"/>
 			Then, he and <a href='https://www.twitter.com/jacuzzijo' target='_blank'>Joses Ho</a> created the DABEST package (Data Analysis with Bootstrap ESTimation) in <a href='https://www.github.com/ACCLAB/DABEST-Python'>Python</a>, <a href='https://www.github.com/dabestr' target='_blank'>R</a> and <a href='https://www.github.com/DABEST-Matlab' target='_blank'>Matlab</a> to make estimation tools more widely available. This web application was built to make estimation available to everyone, including those who haven’t gotten around to learning to code.
 		</p>
-
 		<p>
 			To read more about the authors’ research, please visit the Claridge-Chang <a href='http://www.claridgechang.net/' target='_blank'>lab webpage</a>.
 		</p>
+		<br>
 
 		<h5>The website</h5>
 		<p>
@@ -23,11 +23,12 @@
 			The web application was built in <a href="https://flask.pocoo.org" target="_blank">Flask</a> and <a href='https://vuejs.org/'>Vue.js</a> by Hung Nguyen.
 		</p>
 		<p>
-			The layout uses fonts and typography designed by <a href='https://practicaltypography.com/end-credits.html#bio' target="_blank">Matthew Butterick</a>. For the exemplar plots, typefaces designed by <a href='https://cargocollective.com/jonnypinhorn' target='blank'>Jonny Pinhorn</a> were used.
+			The layout uses fonts and typography designed by <a href='https://practicaltypography.com/end-credits.html#bio' target="_blank">Matthew Butterick</a>. Typefaces designed by <a href='https://cargocollective.com/jonnypinhorn' target='blank'>Jonny Pinhorn</a> were used in the exemplar plots.
 		</p>
 		<p>
 			This site is © 2017-2018 Adam Claridge-Chang and Joses Ho. All rights reserved.
 		</p>
+		<br>
 
 		<h5>How to cite</h5>
 		<p>
@@ -37,6 +38,7 @@
 			<br>
 			<a href='https://doi.org/10.1101/377978' target='_blank'>https://doi.org/10.1101/377978</a>
 		</p>
+		<br>
 
 		<h5>Contact</h5>
 		<p>

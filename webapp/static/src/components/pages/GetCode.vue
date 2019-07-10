f5155906f1db972005e303f762087093a4881004
Joses W. Ho <joseshowh@gmail.com>
2019-07-10 15:47:06 +0800

    remove title

--- webapp/static/src/components/pages/GetCode.vue
+++ webapp/static/src/components/pages/GetCode.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
-		<h5>The DABEST packages</h5>
-
+		
 		<a href='https://github.com/ACCLAB/dabestr'><img src='https://img.shields.io/badge/R-dabestr--0.2.2-green.svg?longCache=true&style=for-the-badge' style="margin:10px 10px"></a>
 
 		<a href='https://github.com/ACCLAB/DABEST-python'><img src='https://img.shields.io/badge/Python-DABEST--0.2.4-blue.svg?longCache=true&style=for-the-badge' style="margin:10px 10px"></a>
